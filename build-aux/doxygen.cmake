e29501489f949f74534b2593fc86e23334698ee6
bunker <bunker@bunkerprivate.com>
2008-12-15 23:30:00 +0000

    Doxygen building improvments, remove some nonportable stuff.

--- build-aux/doxygen.cmake
+++ build-aux/doxygen.cmake
@@ -1,12 +1,6 @@
-function(add_doxygen template_file doxyfile_overrides extra_commands)
+function(add_doxygen target_name template_file extra_commands)
   find_package(Doxygen)
   find_package(LATEX)
-
-  set(doxyfile ${CMAKE_BINARY_DIR}/doc/Doxyfile)
-
-  file(READ ${template_file} file_contents)
-  file(WRITE ${doxyfile} ${file_contents})
-  file(APPEND ${doxyfile} "${extra_commands}")
 
   # TODO: check confs has a couple of required fields defined.
   # TODO: if latex is enabled, then add the latex makefile to make doxygen - we need to 
@@ -14,19 +8,8 @@ function(add_doxygen template_file doxyfile_overrides extra_commands)
   # TODO: if latex, find dvi2ps and all the other crap it needs; make as well.
   # TODO: we must deal with it gracefully if if all breaks massively (have
   #         an overrides-overrides maybe?  Or use echo?)
-  #         
-  # arg there's a problem again  - if the user overrides contain things like 
-  # latex and other stuff doesn't, everthing is broken again.  Maybe we have
-  # to force options like that to be configured through the build system.  OR just
-  # say inthereadme - if you make depenadncy findingstuff  break, then re-do it.
-  # I should put the paths in the doxyfile  - that would probably sort it.
-  # 
-  # 
-  # Ehh... it's easier to get the user to supply the template doxyfile
-  # instead and then we always have control of it.  In fact it's even  easier to
-  # just use echo.  Win has this at least; forget all this file building rubbish.
-  # We can then always have exclusive control over whether latex is on (or whatever
-  # else).
+  # TODO: set dot path if WANT_DOXYGEN_GRAPHICS (blah blah put in ccmakevars)
+  #       how do you default a var?!?!
 
   if (UNIX)
     find_program(CAT_EXE cat REQUIRED)
@@ -40,7 +23,7 @@ function(add_doxygen template_file doxyfile_overrides extra_commands)
   # TODO: if cat_exe not found, then just generate from the doxyfile (but 
   #       also print an error)
 
-  # TODO: obv. this is broken.
+  # TODO: obv. this is broken - need to know the outputdir first
   set(latex_directory ${CMAKE_BINARY_DIR}/doc/latex/)
 
   # TODO: only do this is latex is enabled
@@ -51,8 +34,8 @@ function(add_doxygen template_file doxyfile_overrides extra_commands)
 #   doxygen-pdf, but  only if latex is enabled.
 
   add_custom_target(
-    doxygen
-    COMMAND ${CAT_EXE} ${doxyfile} ${doxyfile_overrides} | ${DOXYGEN_EXECUTABLE} -
+    ${target_name}
+    COMMAND "( ${CAT_EXE} ${template_file}; echo ${extra_commands} ) | ${DOXYGEN_EXECUTABLE} -"
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
   )
     
